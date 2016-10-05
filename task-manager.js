2dd0ab0dd2fb56b58ff6cc806693d8fc3f8d8f4d
LGordienkova <l.gordienkova@gmail.com>
2016-10-05 21:30:51 +0300

    Update task-manager.js

--- task-manager.js
+++ task-manager.js
@@ -1,1 +1,27 @@
-angular.module('taskManager', []);
+angular.module('taskManager', []).controller('MainController', function(){
+	$scope.tasks = [];
+	$scope.showDone = false;
+	$scope.sort = ['urgency', 'importance'];
+	$scope.activeTask = null;
+
+	this.createNew = function(){
+		$scope.activeTask = {
+			description: '',
+			urgency: true,
+			importance: true,
+			done: false,
+			created: new Date()
+		};
+		$scope.tasks.unshift($scope.activeTask);
+	}
+	this.setSort = function(sort){
+		if (sort == 'urgency') $scope.sort = ['urgency', 'importance', 'created'];
+		else if (sort == 'importance') $scope.sort = ['urgency', 'importance', 'created'];
+	}
+	this.toggleShowDone = function(){
+		$scope.showDone = !$scope.showDone;
+	}
+	this.completeTask = function(task){
+		task.done = true;
+	}
+});
